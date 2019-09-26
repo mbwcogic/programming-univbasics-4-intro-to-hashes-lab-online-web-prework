161b88752d36e10dd16478951168ff8f973a6932
Michael Waldo <mbwcogic@yahoo.com>
2019-09-26 02:00:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
+  second_new_hash = Hash.new
   my_hash_creator :key, value
 # return a hash that includes the key and value parameters passed into this method
 end
