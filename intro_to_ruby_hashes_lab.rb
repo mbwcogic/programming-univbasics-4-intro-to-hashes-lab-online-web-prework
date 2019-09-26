b3930756bd29d7e6d7eae460e8572ad781ffce48
Michael Waldo <mbwcogic@yahoo.com>
2019-09-26 02:04:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = Hash.new
+  my_hash_creator(:name=>"Grace Hopper")
   
 # return a hash that includes the key and value parameters passed into this method
 end
