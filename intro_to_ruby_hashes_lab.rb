6b6ad641e2679cbd7a2dcf2570130647102c201d
Michael Waldo <mbwcogic@yahoo.com>
2019-09-26 02:50:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,9 @@ end
 
  def my_hash_creator(key, value)
   my_hash_creator:name, 'Grace Hopper'
-
-# return a hash that includes the key and value parameters passed into this method
 end
+# return a hash that includes the key and value parameters passed into this method
+
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
